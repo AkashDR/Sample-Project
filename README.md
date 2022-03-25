eee8e7ee5f2d8ed52213b8e7b4d09fab94124438
AkashDR <akashdandur@gmail.com>
2022-03-25 12:16:51 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-yohtxf
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-yohtxf)
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Akash D R</h1>
+      <h3>Works at UST Global</h3>
+      <p>Reach me at akashdandur@gmail.com</p>
+    </div>
+  );
+}
